dc839b1b7dfa933c41900532e46ec422dc885b71
D. Bohdan <dbohdan@dbohdan.com>
2021-04-23 08:51:00 +0000

    refactor(example/echo-tcl): use [namespace import]

--- example/echo-tcl.tcl
+++ example/echo-tcl.tcl
@@ -14,45 +14,54 @@
 
 package require Fcgi
 
-proc printEnv {label envArrayName} {
-    upvar $envArrayName envArray
-    fcgi::puts "$label:<br>\n<pre>"
-    foreach name [lsort [array names envArray]] {
-        fcgi::puts "$name=$envArray($name)"
-    }
-    fcgi::puts "</pre><p>"
+namespace eval echo-tcl {
+    namespace path ::fcgi
 }
 
-foreach name [array names env] {
-    set initialEnv($name) $env($name)
-}
-set count 0
-while {[FCGI_Accept] >= 0 } {
-    incr count
-    fcgi::puts -nonewline "Content-type: text/html\r\n\r\n"
-    fcgi::puts "<title>FastCGI echo (Tcl)</title>"
-    fcgi::puts "<h1>FastCGI echo (Tcl)</h1>"
-    fcgi::puts "Request number $count <p>"
-    if [info exists env(CONTENT_LENGTH)] {
-        set len $env(CONTENT_LENGTH)
-    } else {
-        set len 0
+proc echo-tcl::printEnv {label envArrayName} {
+    upvar $envArrayName envArray
+    puts "$label:<br>\n<pre>"
+    foreach name [lsort [array names envArray]] {
+        puts "$name=$envArray($name)"
     }
-    if {$len == 0} {
-        fcgi::puts "No data from standard input.<p>"
-    } else {
-        fcgi::puts "Standard input:<br>\n<pre>"
-        for {set i 0} {$i < $len} {incr i} {
-            set ch [fcgi::read stdin 1]
-            if {$ch == ""} {
-                fcgi::puts -nonewline "Error: Not enough bytes received "
-                fcgi::puts "on standard input<p>"
-                break
-	    }
-            fcgi::puts -nonewline $ch
-	}
-        fcgi::puts "\n</pre><p>"
-    }
-    printEnv "Request environment" env
-    printEnv "Initial environment" initialEnv
+    puts "</pre><p>"
 }
+
+proc echo-tcl::main {} {
+    global env
+    array set initialEnv [array get env]
+
+    set count 0
+
+    while {[FCGI_Accept] >= 0 } {
+        incr count
+        puts -nonewline "Content-type: text/html\r\n\r\n"
+        puts "<title>FastCGI echo (Tcl)</title>"
+        puts "<h1>FastCGI echo (Tcl)</h1>"
+        puts "Request number $count <p>"
+        if [info exists env(CONTENT_LENGTH)] {
+            set len $env(CONTENT_LENGTH)
+        } else {
+            set len 0
+        }
+        if {$len == 0} {
+            puts "No data from standard input.<p>"
+        } else {
+            puts "Standard input:<br>\n<pre>"
+            for {set i 0} {$i < $len} {incr i} {
+                set ch [read stdin 1]
+                if {$ch == ""} {
+                    puts -nonewline "Error: Not enough bytes received "
+                    puts "on standard input<p>"
+                    break
+                }
+                puts -nonewline $ch
+            }
+            puts "\n</pre><p>"
+        }
+        printEnv "Request environment" env
+        printEnv "Initial environment" initialEnv
+    }
+}
+
+echo-tcl::main
